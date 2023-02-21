5d57a805c3fbfca5d2a498f48c9701777a730338
CarlosPrieto <carlosprieto66@icloud.com>
2023-02-20 17:46:48 -0800

    Character background flashes when attacked

--- src/components/GameDisplay.vue
+++ src/components/GameDisplay.vue
@@ -3,13 +3,13 @@
     <div class="battle-display">
       <div class="player-card card">
         <h2>Player</h2>
-        <div class="char-img" :class="{ attacking: playerMove, 'monster-dmg': playerMove}">
+        <div class="char-img" :class="{ attacking: playerMove, 'player-dmg': monsterMove}">
           <img id="player-image" src="../assets/images/NROL-47_Knight.png" alt="player-image" />
         </div>
       </div>
       <div class="monster-card card">
         <h2>Monster</h2>
-        <div id="monster-image" class="char-img" :class="{ attacked: monsterMove, 'player-dmg': playerMove}">
+        <div id="monster-image" class="char-img" :class="{ attacked: monsterMove, 'monster-dmg': playerMove}">
           <img src="../assets/images/640px-Troll_Wesnoth.png" alt="monster-image" />
         </div>
       </div>
